ba36d908dce6ad31620a5154d493f29ac1792648
Elkana Tovey <elkana.tovey@ibm.com>
2023-08-07 16:39:12 +0300

    basic main runs, start work on relay

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"mbg-relay/relay"
+)
+
+func main() {
+	fmt.Println("random number:", relay.MaintainConnection())
+}
--- relay/relay.go
+++ relay/relay.go
@@ -1,3 +1,5 @@
 package relay
 
 // proxy via which server and client connect
+
+func MaintainConnection() int { return 1 }
