e162ee4dd87a96f3b7901565c5760f4f4ce37b94
Elkana Tovey <elkana.tovey@ibm.com>
2023-08-07 16:39:31 +0300

    realy changes

--- relay/relay.go
+++ relay/relay.go
@@ -1,5 +1,25 @@
 package relay
 
+// "bytes"
+// "crypto/tls"
+// "fmt"
+// "io"
+// "net/http"
+// "time"
+
+// "github.com/sirupsen/logrus"
+
+type Client struct {
+	// client     *jsonapi.Client
+	// basePath   string
+	// objectType reflect.Type
+	// listType   reflect.Type
+}
+
 // proxy via which server and client connect
 
 func MaintainConnection() int { return 1 }
+
+// func ServerConnect() int { return 1 }
+
+// func ClientConnect() int { return 1 }
