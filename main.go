6e73972919c94159c24580460f4e087f48c4dd4f
Elkana Tovey <elkana.tovey@ibm.com>
2023-08-18 15:45:59 +0300

    main: add dummy example server

--- main.go
+++ main.go
@@ -1,10 +1,44 @@
 package main
 
 import (
+	"errors"
+	"net/http"
+	"os"
+	"time"
+
 	"fmt"
 	"mbg-relay/relay"
 )
 
+const serverPort = 3333
+
 func main() {
+
+	go func() {
+		mux := http.NewServeMux()
+		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Printf("server: %s /\n", r.Method)
+		})
+		server := http.Server{
+			Addr:    fmt.Sprintf(":%d", serverPort),
+			Handler: mux,
+		}
+		if err := server.ListenAndServe(); err != nil {
+			if !errors.Is(err, http.ErrServerClosed) {
+				fmt.Printf("error running http server: %s\n", err)
+			}
+		}
+	}()
+	time.Sleep(100 * time.Millisecond)
+	requestURL := fmt.Sprintf("http://localhost:%d", serverPort)
+	res, err := http.Get(requestURL)
+	if err != nil {
+		fmt.Printf("error making http request: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("client: got response!\n")
+	fmt.Printf("client: status code: %d\n", res.StatusCode)
+
 	fmt.Println("random number:", relay.MaintainConnection())
 }
