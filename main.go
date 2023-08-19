db711d75abfcf77ab875d88689e1fb3de2798baa
Elkana Tovey <elkana.tovey@ibm.com>
2023-08-19 03:49:26 +0300

    relay: start work on server connect side main: refactor relay's server into calls

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"net/http"
 	"os"
 	"time"
@@ -10,27 +9,12 @@ import (
 	"mbg-relay/relay"
 )
 
-const serverPort = 3333
-
 func main() {
 
-	go func() {
-		mux := http.NewServeMux()
-		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			fmt.Printf("server: %s /\n", r.Method)
-		})
-		server := http.Server{
-			Addr:    fmt.Sprintf(":%d", serverPort),
-			Handler: mux,
-		}
-		if err := server.ListenAndServe(); err != nil {
-			if !errors.Is(err, http.ErrServerClosed) {
-				fmt.Printf("error running http server: %s\n", err)
-			}
-		}
-	}()
+	go relay.StartRelay()
+
 	time.Sleep(100 * time.Millisecond)
-	requestURL := fmt.Sprintf("http://localhost:%d", serverPort)
+	requestURL := fmt.Sprintf("http://localhost:%d/clientconn", relay.ServerPort)
 	res, err := http.Get(requestURL)
 	if err != nil {
 		fmt.Printf("error making http request: %s\n", err)
--- relay/relay.go
+++ relay/relay.go
@@ -12,22 +12,26 @@
 package relay
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
-const serverPort = 3333
+const ServerPort = 3333
 
 // StartRelay starts the main relay function.
 // Responsibilities: start listener for servers, start listeners for clients
 func StartRelay() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/serverconn", HandleServerLongTermConnection)
-	mux.HandleFunc("/clientconn", HandleClientConnection)
-	mux.HandleFunc("/servercallback", HandleServerCallBackConnection)
+	mux.HandleFunc("/serverconn", HandleServerLongTermConnection)     //listen
+	mux.HandleFunc("/clientconn", HandleClientConnection)             //call
+	mux.HandleFunc("/servercallback", HandleServerCallBackConnection) //accept
+
 	server := http.Server{
-		Addr:    fmt.Sprintf(":%d", serverPort),
+		Addr:    fmt.Sprintf(":%d", ServerPort),
 		Handler: mux,
 	}
 	if err := server.ListenAndServe(); err != nil {
@@ -37,8 +41,73 @@ func StartRelay() {
 	}
 }
 
+// under construction
 func HandleServerLongTermConnection(w http.ResponseWriter, r *http.Request) {
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		http.Error(w, "SSE not supported", http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println("server connected to relay...")
+
+	w.Header().Set("Content-Type", "text/event-stream")
+
+	conReqCh := make(chan string)
+
+	// @todo put channel + r.Context in db that client conns can access
+
+	for connectionRequest := range conReqCh {
+		event, err := formatServerSentEvent("connection-request", connectionRequest)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+
+		_, err = fmt.Fprint(w, event)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+
+		flusher.Flush()
+	}
+
 	fmt.Printf("server: %s /\n", r.Method)
+}
+
+// formatServerSentEvent takes name of an event and any kind of data and transforms
+// into a server sent event payload structure.
+// Data is sent as a json object, { "data": <your_data> }.
+//
+// Example:
+//
+//	Input:
+//		event="connection-request"
+//		data=servicefoo
+//	Output:
+//		event: connection-request\n
+//		data: "{\"data\":servicefoo}"\n\n
+func formatServerSentEvent(event string, data any) (string, error) {
+	m := map[string]any{
+		"data": data,
+	}
+
+	buff := bytes.NewBuffer([]byte{})
+
+	encoder := json.NewEncoder(buff)
+
+	err := encoder.Encode(m)
+	if err != nil {
+		return "", err
+	}
+
+	sb := strings.Builder{}
+
+	sb.WriteString(fmt.Sprintf("event: %s\n", event))
+	sb.WriteString(fmt.Sprintf("data: %v\n\n", buff.String()))
+
+	return sb.String(), nil
 }
 
 func HandleClientConnection(w http.ResponseWriter, r *http.Request) {
