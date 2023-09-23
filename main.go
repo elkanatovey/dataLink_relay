2b5964383b646fa6ddb8af5f2bd5f31c87833d0d
Elkana Tovey <elkana.tovey@ibm.com>
2023-09-23 21:36:03 +0300

    main.go: update example so fails later

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"net/http"
 	"os"
 	"time"
@@ -9,20 +12,43 @@ import (
 	"mbg-relay/relayconn"
 )
 
+const ServerPort = 3333
+
+// StartRelay starts the main relay function.
+// Responsibilities: start listener for servers, start listeners for clients
+func StartRelay() { //@todo currently incorrect
+	r := relayconn.NewRelay()
+	server := http.Server{
+		Addr:    fmt.Sprintf(":%d", ServerPort),
+		Handler: r.Mux,
+	}
+	if err := server.ListenAndServe(); err != nil {
+		if !errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("error running http server: %s\n", err)
+		}
+	}
+}
+
 func main() {
 
-	go relayconn.StartRelay()
+	go StartRelay()
 
 	time.Sleep(100 * time.Millisecond)
-	requestURL := fmt.Sprintf("http://localhost:%d/clientconn", relayconn.ServerPort)
-	res, err := http.Get(requestURL)
+	requestURL := fmt.Sprintf("http://localhost:%d%s", ServerPort, relayconn.Dial)
+	cr := relayconn.ConnectionRequest{"a", "123", "456"}
+	reqBodyBytes, _ := json.Marshal(cr)
+	req, err := http.NewRequest("GET", requestURL, bytes.NewReader(reqBodyBytes))
+	client := &http.Client{}
+	response, err := client.Do(req)
+
+	//res, err := http.Get(requestURL)
 	if err != nil {
 		fmt.Printf("error making http request: %s\n", err)
 		os.Exit(1)
 	}
 
 	fmt.Printf("client: got response!\n")
-	fmt.Printf("client: status code: %d\n", res.StatusCode)
+	fmt.Printf("client: status code: %d\n", response.StatusCode)
 
 	fmt.Println("random number:", relayconn.MaintainConnection())
 }
